a232ed9e865e72802b9509c10be40632a38debd4
SuperSLD <37121925+SuperSLD@users.noreply.github.com>
2022-03-23 07:40:12 +0300

    Добавил сохранение настроек

---
+++ MaiDesktop/data/appsettingsrepository.cpp
@@ -0,0 +1,29 @@
+#include "appsettingsrepository.h"
+
+AppSettingsRepository::AppSettingsRepository() {
+     settings = new QSettings("settings.ini", QSettings::IniFormat);
+}
+
+AppSettingsRepository::~AppSettingsRepository() {
+    delete settings;
+}
+
+
+void AppSettingsRepository::saveGroup(GroupModel group) {
+    settings->setValue("group_id", group.getId());
+    settings->setValue("group_course", group.getCourse());
+    settings->setValue("group_fac", group.getFaculty());
+    settings->setValue("group_name", group.getName());
+    settings->setValue("group_level", group.getLevel());
+    settings->sync();
+}
+
+GroupModel AppSettingsRepository::getGroup() {
+    return GroupModel(
+        settings->value("group_id", "").toString(),
+        settings->value("group_name", "").toString(),
+        settings->value("group_fac", "").toString(),
+        settings->value("group_level", "").toString(),
+        settings->value("group_course", "0").toInt()
+    );
+}
---
+++ MaiDesktop/data/appsettingsrepository.h
@@ -0,0 +1,11 @@
+#ifndef APPSETTINGSREPOSITORY_H
+#define APPSETTINGSREPOSITORY_H
+
+
+class AppSettingsRepository
+{
+public:
+    AppSettingsRepository();
+};
+
+#endif // APPSETTINGSREPOSITORY_H
